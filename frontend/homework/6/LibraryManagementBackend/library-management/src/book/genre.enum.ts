1dc6adb453afb28f9a9f22912e1c9ddf6a1e42dc
sanidhyagupta-web <sanidhya.gupta@kickdrumtech.com>
2026-02-09 13:11:12 +0530

    Delete frontend/homework/6/LibraryManagementBackend/library-management directory

--- frontend/homework/6/LibraryManagementBackend/library-management/src/book/genre.enum.ts
+++
@@ -1,9 +0,0 @@
-export enum Genre {
-  FICTION = 'Fiction',
-  NON_FICTION = 'Non-Fiction',
-  FANTASY = 'Fantasy',
-  SCIENCE = 'Science',
-  HISTORY = 'History',
-  BIOGRAPHY = 'Biography',
-  MYSTERY = 'Mystery',
-}
